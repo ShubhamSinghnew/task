d5e30e1be4cb0efcb484a314993ef115a473c4a1
ShubhamSinghnew <124259343+ShubhamSinghnew@users.noreply.github.com>
2023-10-22 00:16:02 +0530

    Update user.js

--- router/user.js
+++ router/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {addUser , createTaks, deleteTask, login , readTask, Update , } from "../controller/user.js"
+import {addUser , createTaks, deleteTask, login , readTask, UpdateTask} from "../controller/user.js"
 import { verifyToken } from '../helper.js';
 const router = express.Router();   
 
@@ -8,11 +8,11 @@ router.post('/api/login', verifyToken, login);
 
 router.post("/api/createTask",verifyToken,createTaks)
 
-router.get("/api/readTask/:userId",readTask)
+router.get("/api/readTask/:userId",verifyToken,readTask)
 
 
 //we use patch here also but i use updateOne function here so i use post 
-router.post("/api/UpdateTask",verifyToken,Update)
+router.post("/api/UpdateTask",verifyToken,UpdateTask)
 
 //we use delete here also but i use deleteOne in function here so i use post 
 router.post("/api/deleteTask",verifyToken,deleteTask)
