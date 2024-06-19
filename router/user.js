e5334f58d1ac2e348275b4825efc1db078921754
ShubhamSinghnew <124259343+ShubhamSinghnew@users.noreply.github.com>
2024-06-19 13:41:30 +0530

    Delete router directory

--- router/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {addUser, login} from "../controller/user.js"
-import { verifyToken } from '../helper.js';
-const router = express.Router();   
-
-router.post("/api/addUser",addUser)
-router.post('/api/login', verifyToken, login);
-
-export default router;
